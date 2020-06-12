0f52abf3b4ef0eac28b4885ecb9fe25adf043f43
Karthik Venkatesan <karthikvenkatesan.kv@gmail.com>
2020-06-12 13:52:04 +0200

    Provide thrust to ship
    Make ship move when button is held down

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+/// <summary>
+/// Ship in game
+/// </summary>
+public class Ship : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    Vector2 thrustDirection;
+    const float ThrustForce = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        thrustDirection = new Vector2(1, 0);
+
+        // Provides damping when thrust stops being provided
+        rb2d.drag = 2.5f;         
+    }
+
+    // FixedUpdate is called with the frequency of the physics system
+    void FixedUpdate()
+    {
+        // Applies thrust when Thrust button is pressed
+        if (Input.GetAxis("Thrust") > 0)
+        {
+            rb2d.AddForce(ThrustForce * thrustDirection, ForceMode2D.Force);
+        }
+    }
+}
