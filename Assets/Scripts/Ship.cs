89a75f875e27e83d763b59d33e6c0f621dad52dd
Karthik Venkatesan <karthikvenkatesan.kv@gmail.com>
2020-06-19 12:36:34 +0200

    Add screen wrapping
    Add screen wrapping so that when the ship leaves the screen on one side, it re-enters the screen from the opposite side

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+//using System.Numerics;
 using UnityEngine;
 
 /// <summary>
@@ -8,27 +9,75 @@ using UnityEngine;
 /// </summary>
 public class Ship : MonoBehaviour
 {
+    #region Components
+
     Rigidbody2D rb2d;
-    Vector2 thrustDirection;
+
+    #endregion
+
+    #region Fields
+
+    // Magnitude and direction of thrust
     const float ThrustForce = 5;
+    Vector2 thrustDirection = new Vector2(1,0);
+
+    // Radius of circle collider
+    float radius;
+
+    #endregion
+
+    #region Methods
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = gameObject.GetComponent<Rigidbody2D>();
-        thrustDirection = new Vector2(1, 0);
+        // Add drag so that ship does not stop suddenly when thrust is removed
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.drag = 2.5f;
 
-        // Provides damping when thrust stops being provided
-        rb2d.drag = 2.5f;         
+        // Get radius of circle collider
+        radius = GetComponent<CircleCollider2D>().radius;
     }
 
     // FixedUpdate is called with the frequency of the physics system
     void FixedUpdate()
     {
-        // Applies thrust when Thrust button is pressed
+        // Applies thrust when 'Thrust' button is pressed
         if (Input.GetAxis("Thrust") > 0)
         {
             rb2d.AddForce(ThrustForce * thrustDirection, ForceMode2D.Force);
         }
     }
+
+    // OnBecameInvisible is called when the game object is no longer visible by any camera
+    void OnBecameInvisible()
+    {
+        Vector2 position = transform.position;
+
+        // Horizontal wrapping
+        if (position.x > ScreenUtils.ScreenRight)
+        {
+            position.x = ScreenUtils.ScreenLeft - radius;
+            transform.position = position;
+        }
+        else if (position.x < ScreenUtils.ScreenLeft)
+        {
+            position.x = ScreenUtils.ScreenRight + radius;
+            transform.position = position;
+        }
+
+        // Vertical wrapping
+        if (position.y > ScreenUtils.ScreenTop)
+        {
+            position.y = ScreenUtils.ScreenBottom - radius;
+            transform.position = position;
+        }
+        else if (position.y < ScreenUtils.ScreenBottom)
+        {
+            position.y = ScreenUtils.ScreenTop + radius;
+            transform.position = position;
+        }
+    }
+
+    #endregion
 }
