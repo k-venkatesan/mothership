f05782e964621d9485ae06c36f073823b9af26a7
Karthik Venkatesan <karthikvenkatesan.kv@gmail.com>
2020-07-16 11:56:32 +0200

    Modify bullet behaviour
    Add screen wrapping to bullet; Destroy bullet after given duration

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    Timer bulletTimer;
+    const float BulletLifeInSeconds = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitializeTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MonitorBulletLife();
     }
 
+    /// <summary>
+    /// Applies impulse force to bullet in given direction
+    /// </summary>
+    /// <param name="forceDirection">Direction in which impulse force is to be applied</param>
     public void ApplyForce(Vector2 forceDirection)
     {
         const float LaunchForce = 10;
         GetComponent<Rigidbody2D>().AddForce(LaunchForce * forceDirection, ForceMode2D.Impulse);
+    }
+
+    /// <summary>
+    /// Sets timer up and runs it
+    /// </summary>
+    void InitializeTimer()
+    {
+        bulletTimer = GetComponent<Timer>();
+        bulletTimer.Duration = BulletLifeInSeconds;
+        bulletTimer.Run();
+    }
+
+    /// <summary>
+    /// Checks timer and destroys bullet if time has elapsed
+    /// </summary>
+    void MonitorBulletLife()
+    {
+        if (bulletTimer.Finished)
+        {
+            Destroy(gameObject);
+        }
     }
 }
